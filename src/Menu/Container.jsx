22ef824404937f51b5ab134a72c60d89fa662e62
whysosergious <sergio.grey@gmail.com>
2021-01-03 13:21:27 +0100

    1

--- src/Menu/Container.jsx
+++ src/Menu/Container.jsx
@@ -5,6 +5,13 @@ import React from 'react';
 import './Container.css';
 import Button from 'shared/Button';
 
+// section media
+import tipsImage from 'ass/img/ess/menu/tips.jpg';
+import lunchImage from 'ass/img/ess/menu/lunch.jpg';
+import foodImage from 'ass/img/ess/menu/food.jpg';
+import tapImage from 'ass/img/ess/menu/tap.jpg';
+import drinksImage from 'ass/img/ess/menu/drinks.jpg';
+
 const MenuContainer = props => {
    return (
       <section className="Menu-Container accent">
@@ -20,18 +27,28 @@ const MenuContainer = props => {
 
 			<Button type="menu"
 				text="Dagens Tips"
+				imgSrc={ tipsImage }
+				imgDesc="Kvällens alternativt veckans special rätter"
 			/>
 			<Button type="menu"
 				text="Lunch"
+				imgSrc={ lunchImage }
+				imgDesc="Risotto som lunchrätt"
 			/>
 			<Button type="menu"
 				text="A la Carté"
+				imgSrc={ foodImage }
+				imgDesc="A la Carté rätt"
 			/>
 			<Button type="menu"
 				text="På Tapp"
+				imgSrc={ tapImage }
+				imgDesc="Barens tapptorn med öl pump-klipps"
 			/>
 			<Button type="menu"
 				text="Öl & Dryck"
+				imgSrc={ drinksImage }
+				imgDesc="Blandade flaskor och burkar"
 			/>
       </section>
    );
